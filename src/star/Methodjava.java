64391127ef59bb1382729483b41e2428e71395a7
SGSTAR01 <startroidx@gmaill.com>
2022-09-12 15:18:57 +0530

    Calculator using Method

--- src/star/Methodjava.java
+++ src/star/Methodjava.java
@@ -1,10 +1,27 @@
 package star;
-
+import java.util.Scanner;
 public class Methodjava {
-    static  void newmethod(String name){
-        System.out.println("Hello "+name);
+    static  int sum(int num1,int num2){
+        return num1+num2;
+    }
+    static  int subtract(int num1,int num2){
+        return num1-num2;
+    }
+    static  int multiply(int num1,int num2){
+        return num1*num2;
+    }
+    static  int divide(int num1,int num2){
+        return num1/num2;
     }
     public static void main(String[] args) {
-        newmethod("Rohan");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 1st Number:");
+        int x=input.nextInt();
+        System.out.println("Enter 2nd Number:");
+        int y=input.nextInt();
+        System.out.println("Addition:"+sum(x,y));
+        System.out.println("Subtraction:"+subtract(x,y));
+        System.out.println("Multiplication:"+multiply(x,y));
+        System.out.println("Division:"+divide(x,y));
     }
 }
