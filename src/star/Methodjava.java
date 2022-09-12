5a5d50d17233083bb4cbd29bd1a0a296a5cc033e
SGSTAR01 <startroidx@gmaill.com>
2022-09-12 12:52:42 +0530

    Method example

---
+++ src/star/Methodjava.java
@@ -0,0 +1,10 @@
+package star;
+
+public class Methodjava {
+    static  void newmethod(String name){
+        System.out.println("Hello "+name);
+    }
+    public static void main(String[] args) {
+        newmethod("Rohan");
+    }
+}
